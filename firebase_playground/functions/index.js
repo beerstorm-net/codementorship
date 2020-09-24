42b3b376791e67094d06968c1e3e5fbcd2a3be67
Yilmaz Guleryuz <397125+zeusbaba@users.noreply.github.com>
2020-09-24 12:14:25 +0200

    update cloud functions

--- firebase_playground/functions/index.js
+++ firebase_playground/functions/index.js
@@ -5,6 +5,7 @@ const runtimeOpts = {
     timeoutSeconds: 300,
     memory: '256MB'
 }
+const uuid = require("uuid");
 
 // The Firebase Admin SDK to access Cloud Firestore.
 const firebaseAdmin = require('firebase-admin');
@@ -22,11 +23,24 @@ exports.newFirestoreRecord = functions
         // perform desired operations ...
         // as an example: lets create this record on realtime db as well
         const db = firebaseAdmin.database();
-        const ref = db.ref("https://codementorship.firebaseio.com/");
-        const dbRef = ref.child("codementorship");
+        const ref = db.ref();
+
+        // NB! child is the docId of the record we are inserting!
+        let recordId = newRecord.id;
+        if (recordId === undefined) {
+            recordId = uuid.v4();
+        }
+        const dbRef = ref.child(recordId);
         await dbRef.set({
-            docId: newRecord
+            newRecord
+        }, function(error) {
+            if (error) {
+                console.log('Error while setting data', error)
+            } else {
+                console.log('all is well done')
+            }
         })
+        console.log('DONE')
 
         return true;
     });
